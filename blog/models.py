0ea8dea3263e8d5bd5597e3f6258c36ff2eb39a1
Ali Fayzi <a.fayzi@outlook.com>
2024-08-11 10:16:21 +0330

    update post model and create contact model and migrate to database

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    # image
+    # author
+    title = models.CharField(verbose_name="Title",name="title",primary_key=False,max_length=60)
+    content = models.TextField(verbose_name="Content",name="content",primary_key=False)
+    # tag
+    # category
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now = True)
+    
+    
+    
+class Contact(models.Model):
+    
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    subject = models.CharField(max_length=255)
+    message = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    
